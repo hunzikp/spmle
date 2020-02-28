ce4474fd0b5fec32eb50d329f360888b9c0ec7fb
ncbormann <ncbormann@users.noreply.github.com>
2020-02-28 09:40:49 +0000

    added working script for time plots

--- scripts/MC-TimePlots.R
+++ scripts/MC-TimePlots.R
@@ -10,28 +10,16 @@ getwd()
 ###################################################
 ### Import Spatial MC results
 files <- paste("output/spatial/", dir("output/spatial"), sep="")
+files
+files <- files[c(1:3,5:6)]
 sp <- data.frame()
 for(f in files) {
   print(f)
   load(f)
-  sp <- rbind(te, results_tb[,c("method", "N", "TT", "time")])
+  sp <- rbind(sp, results_tb[!is.na(results_tb$rho_se),c("method", "N", "time", "rho_se")])
 }
 rm(files, f, results_tb)
-
-## N=256
-sp <- read.csv("output/spatial/mcresults-256n-500r-200127.csv", header=T, sep=",")
-## N=1024
-load("output/spatial/mcresults-sp-1024n-500r-200127.Rdata")
-sp <- rbind(sp,results_tb)
-rm(results_tb)
-## N=4096
-load("output/spatial/mcresults-sp-4096n-500r-200128.Rdata")
-sp <- rbind(sp,results_tb)
-rm(results_tb)
-## N=16384
-load("output/spatial/mcresults-sp-16384n-250r-200128.Rdata")
-sp <- rbind(sp,results_tb)
-rm(results_tb)
+unique(paste(sp$method, sp$N, sep="-"))
 
 ### Import Temporal MC results
 files <- paste("output/temporal/", dir("output/temporal"), sep="")
@@ -39,21 +27,23 @@ te <- data.frame()
 for(f in files) {
   print(f)
   load(f)
-  te <- rbind(te, results_tb[,c("method", "N", "TT", "time")])
+  te <- rbind(te, results_tb[!is.na(results_tb$rho_se),c("method", "N", "TT", "time")])
 }
 rm(files, f, results_tb)
+unique(paste0(te$method, st$N, st$TT))
 
 ### Import Spatio-Temporal MC results
 files <- paste("output/spatio-temporal/", dir("output/spatio-temporal"), sep="")
 st <- data.frame()
 for(f in files) {
-  print(f)
-  load(f)
-  st <- rbind(st, results_tb[,c("method", "N", "TT", "time")])
+  if(f!="mcresults-ST-49n20t-RIS-100r-200212.Rdata"){
+    print(f)
+    load(f)
+    st <- rbind(st, results_tb[!is.na(results_tb$rho_se), c("method", "N", "TT", "time")])
+  }
 }
 rm(files, f, results_tb)
-st <- st[st$N!=49,]
-
+unique(paste0(st$method, st$N, st$TT))
 
 ###################################################
 ### Plot times
@@ -79,7 +69,7 @@ ggsave(filename="plots/MC_sp-time-200219.pdf")
 # Prep
 #te$NT <- as.factor(paste(te$N*te$TT, " (", te$N, " x ", te$TT, ")", sep=""))
 te$NT <- as.factor(paste(te$N, te$TT, sep=" x "))
-te$NT <- ordered(te$NT, levels = c("64 x 4", "64 x 16"))
+te$NT <- ordered(te$NT, levels = c("64 x 4", "64 x 16", "256 x 16"))
 te$Estimator <- as.factor(te$method)
 levels(te$Estimator)
 levels(te$Estimator) <- list(RIS="ris", SPMLE="spmle")
@@ -88,17 +78,17 @@ levels(te$Estimator) <- list(RIS="ris", SPMLE="spmle")
 ggplot(aes(y = time, x = Estimator, fill = NT, dodge=NT), data=te) +
   geom_boxplot() +
   scale_y_log10(name="Log(Seconds)") +
-  scale_fill_manual(values=c("#F8766D", "#00BA38")) + # change colour of fill
+  scale_fill_manual(values=c("#F8766D", "#00BA38", "#4E84C4")) + # change colour of fill
   ggtitle("Mean Estimation Time for Temporal Monte Carlo Experiments") +
   theme_minimal()
-ggsave(filename="plots/MC_te-time-200219.pdf")
+ggsave(filename="plots/MC_te-time-200228.pdf")
 
 
 ## Spatio-Temporal
 # Prep
 #st$NT <- as.factor(passt(st$N*st$TT, " (", st$N, " x ", st$TT, ")", sep=""))
 st$NT <- as.factor(paste(st$N, st$TT, sep=" x "))
-st$NT <- ordered(st$NT, levels = c("64 x 4", "64 x 16"))
+st$NT <- ordered(st$NT, levels = c("64 x 4", "64 x 16", "256 x 16"))
 st$Estimator <- as.factor(st$method)
 levels(st$Estimator)
 levels(st$Estimator) <- list(RIS="ris", SPMLE="spmle")
@@ -107,10 +97,10 @@ levels(st$Estimator) <- list(RIS="ris", SPMLE="spmle")
 ggplot(aes(y = time, x = Estimator, fill = NT, dodge=NT), data=st) +
   geom_boxplot() +
   scale_y_log10(name="Log(Seconds)") +
-  scale_fill_manual(values=c("#F8766D", "#00BA38")) + # change colour of fill
+  scale_fill_manual(values=c("#F8766D", "#00BA38", "#4E84C4")) + # change colour of fill
   ggtitle("Mean Estimation Time for Spatio-Temporal Monte Carlo Experiments") +
   theme_minimal()
-ggsave(filename="plots/MC_st-time-200219.pdf")
+ggsave(filename="plots/MC_st-time-200228.pdf")
 
 
 
