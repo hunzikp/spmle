5f7bf3cdfe63231f9259d9c85d9d090c5c483c28
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-03 22:02:18 +0800

    Delete R directory

--- R/pyinstall.R
+++
@@ -1,7 +0,0 @@
-##############################################################
-# Installation function for python dependencies
-##############################################################
-
-install_pydep <- function(method = "auto", conda = "auto") {
-  reticulate::py_install("scipy", method = method, conda = conda)
-}
